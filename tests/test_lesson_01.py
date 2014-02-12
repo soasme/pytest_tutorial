7b0f6ec9dfd31c07b48cf834ce0bf922838e7ef9
linju <linju@douban.com>
2014-02-12 08:36:17 +0800

    Test 2 cases for lesson 1.

--- tests/test_lesson_01.py
+++ tests/test_lesson_01.py
@@ -6,7 +6,7 @@ Fixture.
 import pytest
 import datetime
 
-from lib.time import mtimeformat
+from lib.time import mtimeformat, yesterdaytime
 
 delta = datetime.timedelta
 now = datetime.datetime.now
@@ -17,5 +17,8 @@ def just_now():
     # TODO Please return a valid time (< 60 seconds)
     pass
 
-def test_just_now(just_now):
+def test_mtime_format_just_now(just_now):
     assert mtimeformat(just_now) == '刚刚'
+
+def test_yesterdaytime_format_just_now(just_now):
+    assert yesterdaytime(just_now) == '今天'
